b45df53c475144687ee6a789c9a36bdd87e0ff65
IvanTheSchizo <ivanangnata123@gmail.com>
2026-09-09 10:55:34 +0700

    Initial commit: Laravel setup and local database connection

---
+++ database/factories/ClassInfoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ClassInfo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ClassInfo>
+ */
+class ClassInfoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/StudentGradeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\StudentGrade;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<StudentGrade>
+ */
+class StudentGradeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
